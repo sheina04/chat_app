2300d88065180a12014be2e82f5599ff32e99d88
sheina <sheinatandugon3@gmail.com>
2025-05-30 11:49:26 +0800

    Organizing the styles and colors for the chat page.

--- lib/utils/spaces.dart
+++ lib/utils/spaces.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
-Widget verticalSpacing(double val) => SizedBox(height: val);
+Widget verticalSpacing(double val) => SizedBox(
+  height: val);
 
-Widget horizontalSpacing(double val) => SizedBox(width: val);
+Widget horizontalSpacing(double val) => SizedBox(
+  width: val);
